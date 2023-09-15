799120e38f16fc0094ca3513a28290f7a24f0602
Lane Lu <llu@interprosoft.com>
2023-09-15 20:12:09 +0800

    Create react-native library

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  PropertyEditorExample
+//
+
+import Foundation
---
+++ ios/PropertyEditor.swift
@@ -0,0 +1,8 @@
+@objc(PropertyEditor)
+class PropertyEditor: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
